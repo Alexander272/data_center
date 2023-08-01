5e1f1b37d6a7945e22521d00e882567045dac11d
Alexander272 <46869920+Alexander272@users.noreply.github.com>
2023-08-01 13:57:11 +0500

    fixed week

--- frontend/src/components/Week/Week.tsx
+++ frontend/src/components/Week/Week.tsx
@@ -2,7 +2,13 @@ import { Stack } from '@mui/material'
 import { Day } from './week.style.'
 
 export const Week = () => {
-	const day = new Date().getDate()
+	const d = new Date()
+
+	const y = d.getFullYear()
+	const m = d.getMonth()
+
+	const day = d.getDate()
+	const lastDay = m == 0 ? new Date(y - 1, 11, 0).getDate() : new Date(y, m, 0).getDate()
 
 	const renderDays = () => {
 		const days: JSX.Element[] = []
@@ -10,7 +16,7 @@ export const Week = () => {
 		for (let i = 6; i >= 0; i--) {
 			days.push(
 				<Day active={i == 0} complete={i > 1}>
-					{day - i}
+					{day - i > 0 ? day - i : lastDay + day - i}
 				</Day>
 			)
 		}
